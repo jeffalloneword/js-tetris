06b88ba7f7ed8a4bde6c8c9d055885be36c20664
jeffalloneword <jhyatt00@gmail.com>
2020-05-27 21:43:29 -0400

    add first shape

--- app.js
+++ app.js
@@ -45,4 +45,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   let currentPosition = 4
   let current = theTetrominoes[0][0]
+
+  function draw () {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino')
+    })
+  }
+
+  draw()
 })
